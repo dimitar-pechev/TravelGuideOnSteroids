d6734c2994a5560d21dc04e97c32f271b7b5fe4a
dimitar-pechev <dimpechev@gmail.com>
2017-03-14 00:11:48 +0200

    forms ui updated

--- TravelGuide.Auth/ApplicationUserManager.cs
+++ TravelGuide.Auth/ApplicationUserManager.cs
@@ -31,11 +31,7 @@ namespace TravelGuide.Auth
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 6
             };
 
             // Configure user lockout defaults
