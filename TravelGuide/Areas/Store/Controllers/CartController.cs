9fb8ab77c11f6528bc262c858c9e9fbc1f0a2fce
dimitar-pechev <dimpechev@gmail.com>
2017-03-16 21:31:56 +0200

    store items addable to cookies via ajax

--- TravelGuide/Areas/Store/Controllers/CartController.cs
+++ TravelGuide/Areas/Store/Controllers/CartController.cs
@@ -1,10 +1,14 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using TravelGuide.Services.Store.Contracts;
 
 namespace TravelGuide.Areas.Store.Controllers
 {
     public class CartController : Controller
     {
+        private const string CookieName = "store-items";
         private readonly ICartService cartService;
 
         public CartController(ICartService cartService)
@@ -15,6 +19,19 @@ namespace TravelGuide.Areas.Store.Controllers
         public ActionResult Index()
         {
             return this.View();
+        }
+
+        [HttpPost]
+        public void AddToCart(Guid? itemId, int? quantity)
+        {
+            if (quantity == null || quantity < 1 || quantity > 10)
+            {
+                quantity = 1;
+            }
+
+            var cookiePrev = this.Request.Cookies.Get(CookieName + this.User.Identity.Name);
+            var cookie = this.cartService.WriteCookie(cookiePrev, this.User.Identity.Name, itemId.ToString(), quantity.ToString());
+            this.Response.SetCookie(cookie);
         }
     }
 }
