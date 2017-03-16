13de5bc0abf06962083da835c3983d3fe624ec47
dimitar-pechev <dimpechev@gmail.com>
2017-03-16 17:32:10 +0200

    cart init setup

---
+++ TravelGuide/Areas/Store/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TravelGuide.Areas.Store.Controllers
+{
+    public class CartController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
