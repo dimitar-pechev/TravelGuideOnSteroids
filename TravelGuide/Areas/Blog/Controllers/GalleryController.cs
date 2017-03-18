5c407f20201cc4a7d9699242ddd57f68d1ad9d89
dimitar-pechev <dimpechev@gmail.com>
2017-03-18 15:00:45 +0200

    gallery models and view init

---
+++ TravelGuide/Areas/Blog/Controllers/GalleryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using TravelGuide.Areas.Blog.ViewModels;
+using TravelGuide.Common.Contracts;
+using TravelGuide.Services.Gallery.Contacts;
+
+namespace TravelGuide.Areas.Blog.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly IGalleryImageService galleryService;
+        private readonly IMappingService mappingService;
+
+        public GalleryController(IGalleryImageService galleryService, IMappingService mappingService)
+        {
+            this.galleryService = galleryService;
+            this.mappingService = mappingService;
+        }
+
+        public ActionResult Index()
+        {
+            var images = this.galleryService.GetAllNotDeletedGalleryImagesOrderedByDate();
+            var model = this.mappingService.Map<IEnumerable<GalleryListViewModel>>(images);
+
+            return this.View(model);
+        }
+    }
+}
---
+++ TravelGuide/Areas/Blog/ViewModels/GalleryListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AutoMapper;
+using TravelGuide.Common.Contracts;
+using TravelGuide.Models.Gallery;
+
+namespace TravelGuide.Areas.Blog.ViewModels
+{
+    public class GalleryListViewModel : IMapFrom<GalleryImage>, IHaveCustomMappings
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CommentsCount { get; set; }
+
+        public int LikesCount { get; set; }
+
+        public void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<GalleryImage, GalleryListViewModel>()
+                .ForMember(dest => dest.CommentsCount, src => src.MapFrom(s => s.Comments.Count))
+                .ForMember(dest => dest.LikesCount, src => src.MapFrom(s => s.Likes.Count));
+        }
+    }
+}
